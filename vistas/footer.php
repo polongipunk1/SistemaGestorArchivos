e90c1117b8f3ec8f7bc67828c1fc22a21c6a76c0
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-10-20 15:34:59 -0500

    Navbar mas header y footer

---
+++ vistas/footer.php
@@ -0,0 +1,6 @@
+<script src="../librerias/jquery-3.5.1.min.js"></script>
+<script src="../librerias/bootstrap4/popper.min.js"></script>
+<script src="../librerias/bootstrap4/bootstrap.min.js"></script>
+<script src="../librerias/sweetalert.min.js"></script>
+</body>
+</html>
---
+++ vistas/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">    
+    <link rel="stylesheet" href="../librerias/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" href="../librerias/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../librerias/fontawesome/css/all.css">
+    <title>Gestor</title>
+    
+</head>
+<body>
+    <!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+          <img src="../img/logo.png" alt="" width="50px">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#"><span class="fas fa-home"></span> Inicio
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><span class="far fa-folder"></span> Administrar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><span class="fas fa-file"></span> Reportes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><span class="fas fa-power-off"></span> Salir</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
